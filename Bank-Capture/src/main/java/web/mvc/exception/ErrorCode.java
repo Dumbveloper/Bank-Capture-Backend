eb1e997772885b5a561c1d621eeee60fd12aedf0
ulh1121 <ulh1121@gmail.com>
2023-09-15 14:42:58 +0900

    #8 enum 예외 추가 완료

--- Bank-Capture/src/main/java/web/mvc/exception/ErrorCode.java
+++ Bank-Capture/src/main/java/web/mvc/exception/ErrorCode.java
@@ -10,7 +10,17 @@ import lombok.RequiredArgsConstructor;
 public enum ErrorCode implements EnumModel{
 
     //예시
-    INVALID_CODE(400, "C001", "Invaalid Cond");
+    //회원 로그인 시 발생 가능 예외
+    INVALID_Customer_Login(400, "C001", "존재하지 않는 고객 정보입니다."),
+    INVALID_Customer_Password(400, "C002", "비밀번호가 일치하지 않습니다."),
+
+
+    // 행원 로그인 시 발생 가능 예외
+    INVALID_BANKER_Login(400,"B001","존재하지 않는 행원 정보입니다."),
+    INVALID_BANKER_Password(400,"B002","비밀번호가 일치하지 않습니다."),
+
+    //중복여부 체크
+    DUPLICATE_CUSTOMER(400,"D001","중복된 이메일입니다.");
 
     private int status;
     private String code;
